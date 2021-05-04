0830df329fbd6c95784e4a3a0c2228fff8e174e0
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-05-04 21:23:59 +0100

    Fix indentation

--- models/Event.js
+++ models/Event.js
@@ -1,11 +1,11 @@
 const mongoose = require(`mongoose`);
 
 const eventSchema = mongoose.Schema({
-	name: String,
-	location: String,
-	description: String,
-	date: Number,
-	time: Number,
+    name: String,
+    location: String,
+    description: String,
+    date: Number,
+    time: Number,
 });
 
 module.exports.Event = mongoose.model(`Event`, eventSchema);
--- src/index.js
+++ src/index.js
@@ -27,35 +27,35 @@ eventful.use(morgan(`combined`));
 
 // CRUD operations
 eventful.get(`/`, async (request, response) => {
-	response.send(await Event.find());
+    response.send(await Event.find());
 });
 
 eventful.post(`/`, async (request, response) => {
-	const newEvent = request.body;
-	const event = new Event(newEvent);
-	await event.save();
-	response.send({ message: `New event created.` });
+    const newEvent = request.body;
+    const event = new Event(newEvent);
+    await event.save();
+    response.send({ message: `New event created.` });
 });
 
 eventful.delete(`/:id`, async (request, response) => {
-	await Event.deleteOne({ _id: ObjectId(request.params.id) });
-	response.send({ message: `Event removed.` });
+    await Event.deleteOne({ _id: ObjectId(request.params.id) });
+    response.send({ message: `Event removed.` });
 });
 
 eventful.put(`/:id`, async (request, response) => {
-	await Event.findOneAndUpdate(
-		{ _id: ObjectId(request.params.id) },
-		request.body
-	);
-	response.send({ message: `Event updated.` });
+    await Event.findOneAndUpdate(
+        { _id: ObjectId(request.params.id) },
+        request.body
+    );
+    response.send({ message: `Event updated.` });
 });
 
 eventful.listen(3001, () => {
-	console.log(`Listening on port 3001`);
+    console.log(`Listening on port 3001`);
 });
 
 var database = mongoose.connection;
 database.on(`error`, console.error.bind(console, `connection error: `));
 database.once(`open`, function callback() {
-	console.log(`Database connected!`);
+    console.log(`Database connected!`);
 });
